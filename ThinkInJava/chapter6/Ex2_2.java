ac76224052baa362dc8187475b104fa071ebd0c5
Yucheng-Yan <yyan7935@uni.sydney.edu.au>
2022-07-14 19:56:48 +0800

    thinkinjava

---
+++ ThinkInJava/chapter6/Ex2_2.java
@@ -0,0 +1,20 @@
+// import fun.thinkinjava.chapter6.mypackages.Ex1;
+// import fun.thinkinjava.chapter6.mypackages.Ex2;
+
+// public class Ex2_2 {
+//     public static void main(String[] args) {
+//         Ex1 e = new Ex1();
+//     }
+// }
+
+//Standard Answer
+import net.mindview.simple.*;
+import java.util.*;
+ 
+public class Ex2_2 {
+	public static void main(String[] args) {
+		// Vector v = new Vector(); // ambiquous collision
+		net.mindview.simple.Vector v1 = new net.mindview.simple.Vector();
+		java.util.Vector v2 = new java.util.Vector();
+	}
+}
---
+++ ThinkInJava/chapter6/mypackages/Ex2.java
@@ -0,0 +1,14 @@
+package fun.thinkinjava.chapter6.mypackages;
+
+
+class Ex1{
+    Ex1() {
+        System.out.println("Fuck them all");
+    }
+}
+
+public class Ex2 {
+    public static void main(String[] args) {
+
+    }
+}
