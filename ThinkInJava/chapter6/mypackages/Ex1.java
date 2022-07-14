acd56e626b9938bafad59658faea78324dbfb8dd
Yucheng-Yan <yyan7935@uni.sydney.edu.au>
2022-07-14 14:57:29 +0800

    thinkinjava

--- ThinkInJava/chapter6/mypackages/Ex1.java
+++ ThinkInJava/chapter6/mypackages/Ex1.java
@@ -1,7 +1,7 @@
-package chapter6.mypackages;
+package fun.thinkinjava.chapter6.mypackages;
  
 public class Ex1 {
-    Ex1(){
+    public Ex1(){
         System.out.println("Fuck it");
     }
     public static void main(String[] args) {
